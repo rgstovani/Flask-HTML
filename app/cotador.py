96d5f504764b901c186e5cb3b31280b12642abf6
PC-Ruan <ruan.stovani@gmail.com>
2023-02-15 08:13:56 -0300

    limpando o codigo de cotar moeda

--- app/cotador.py
+++ app/cotador.py
@@ -8,37 +8,3 @@ def cotar(escolha):
     cotacao = requisicao.json()
     moeda = float(cotacao[escolha]['bid'])
     return moeda
-
-#
-# #Cotação da moeda
-# print(f'O valor atual do {moeda_escolhida[escolha]} é ',cotar())
-# while True:
-#     try:
-#         preco_alvo = float(input('Digite o valor desejado: R$'))
-#         cria_linha()
-#         break
-#     except ValueError:
-#         print('Valor Invalido. Use o ponto para casas decimais.')
-#         cria_linha()
-#
-# #tempo de atualização
-# while True:
-#     tempo_de_atualizacao = input('Digite o tempo de atualização (em segundos):')
-#     cria_linha()
-#     if tempo_de_atualizacao.isnumeric():
-#         tempo = int(tempo_de_atualizacao)
-#         break
-#     else:
-#         print('Digite apenas numeros.!')
-#
-# while True:
-#     cotar()
-#     print('Atualizado em: ', datetime.now().strftime("%d-%m-%y %H:%M:%S"))
-#     print('Cotação Atual:', cotar())
-#     if (cotar() <= preco_alvo):
-#         print('Bora comprar!')
-#         hora_de_comprar()
-#     elif (cotar() > preco_alvo):
-#         print('Ainda está caro!')
-#         cria_linha2()
-#     time.sleep(tempo)
